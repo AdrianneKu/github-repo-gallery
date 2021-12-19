cdacc8a5b66cb51a7cbab5dbce78161b1284c1cd
AdrianneKu <adrianne@adriannekuczynski.me>
2021-12-19 12:54:12 -0600

    Updated js file to list repos

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 //overview class
 const overview = document.querySelector(".overview");
 //my GitHub username
-const username = "AdrianneKU";
+const username = "AdrianneKu";
+//ul list location to display repos
+const displayRepos = document.querySelector(".repo-list");
 
 //fetching my GitHub profile   
 const gitHubProfile = async function () {
    const res = await fetch(`https://api.github.com/users/${username}`);
    const data = await res.json();
    displayMyGitHub(data);
-   //console.log(data);
 };
 
 gitHubProfile();
 
+//display my GitHub profile
 const displayMyGitHub = function (data) {
    const div = document.createElement("div");
    div.classList.add("user-info");
@@ -28,4 +30,22 @@ const displayMyGitHub = function (data) {
     </div>
     `;
    overview.append(div);
+   gitHubRepos();
+};
+   
+//fetching my GitHub repos   
+const gitHubRepos = async function () {
+   const repoInfo = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+   const repoData = await repoInfo.json();
+   displayRepoInfo(repoData);
+};
+
+//displaying repo info
+const displayRepoInfo = function (repos) {
+   for (const repo of repos) {
+      const repoItem = document.createElement("li");
+      repoItem.classList.add("repo");
+      repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+      displayRepos.append(repoItem);
    };
+};
